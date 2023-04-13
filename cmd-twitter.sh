9e73385e80dfaca1a312b7cd8da8d001cf9f04e8
Rahul Gill <rgill1@protonmail.com>
2023-04-13 19:58:34 +0530

    added twitter

---
+++ cmd-twitter.sh
@@ -0,0 +1,10 @@
+java -jar cli.jar \
+    -a twitter.apk \
+    -b patches.jar \
+    -m int.apk \
+    -c \
+    -o twitter-ReVanced.apk \
+    --exclusive \
+    -i dynamic-color \
+    -i hide-ads \
+    -i hide-recommended-users \
